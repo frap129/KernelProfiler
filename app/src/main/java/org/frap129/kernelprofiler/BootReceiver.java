5f8298f45d8534931c2087bad05dba118e59eb28
Joe Maples <joe@frap129.org>
2017-05-04 17:35:54 -0400

    Notification: Add intent to lauch Kernel Profiler
    Signed-off-by: Joe Maples <joe@frap129.org>

--- app/src/main/java/org/frap129/kernelprofiler/BootReceiver.java
+++ app/src/main/java/org/frap129/kernelprofiler/BootReceiver.java
@@ -1,10 +1,12 @@
 package org.frap129.kernelprofiler;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.support.v4.app.NotificationCompat;
 
 import java.util.Map;
@@ -32,12 +34,17 @@ public class BootReceiver extends BroadcastReceiver{
                     MainActivity.setProfile(profilePath, context);
                 }
                 if (bootNoti.getBoolean("onBootNoti", true)) {
+                    PackageManager pm = context.getPackageManager();
+                    Intent launchIntent = pm.getLaunchIntentForPackage(BuildConfig.APPLICATION_ID);
+                    PendingIntent contentIntent = PendingIntent.getActivity(context, 0, launchIntent, 0);
                     String profileNotification = "Successfully applied " + prof.getString("profile", "profile") + "!";
                     NotificationCompat.Builder mBuilder =
                             new NotificationCompat.Builder(context)
                                     .setSmallIcon(R.mipmap.ic_notification)
                                     .setContentTitle("Kernel Profiler")
-                                    .setContentText(profileNotification);
+                                    .setContentText(profileNotification)
+                                    .setContentIntent(contentIntent)
+                                    .setAutoCancel(true);
                     int mNotificationId = 1;
                     NotificationManager mNotifyMgr =
                             (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
